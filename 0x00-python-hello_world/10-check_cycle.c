b0d919c61c1c873f7b9eba12c06c929b8a195b5f
davislenny <davidlmwangi7@gmail.com>
2022-11-29 22:55:04 -0800

    Add 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the linked list
+ * Return: 1 in cycle present, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *forward, *backwards;
+
+	forward = list;
+	backwards = list;
+	while (forward->next && backwards)
+	{
+		forward = forward->next->next;
+		backwards = backwards->next;
+		if (forward == backwards)
+			return (1);
+	}
+	return (0);
+}
