be928492c47302b6b46e1773990a8748b6352ce8
davislenny <davidlmwangi7@gmail.com>
2022-12-01 11:02:52 -0800

    Add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    elements = len(sys.argv) - 1
+    if elements == 0:
+        print("0 arguments.")
+    elif elements == 1:
+        print("1 argument:")
+    else:
+        print(f"{elements} arguments")
+
+    for count in range(1, elements + 1):
+        print(f"{count}: {sys.argv[count]}")
