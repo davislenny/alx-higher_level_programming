d01e6a2002a6b699ff490d19eff5a6da7859b28d
davislenny <davidlmwangi7@gmail.com>
2022-12-05 05:17:56 -0800

    Add header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -19,7 +18,7 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t **head);
+void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
