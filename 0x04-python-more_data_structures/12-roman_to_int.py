07d6a2c94aaf106f3408c3f1220ed416fd9f1b3c
davislenny <davidlmwangi7@gmail.com>
2022-12-09 07:41:00 -0800

    Add 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+    roman_string = roman_string.replace("IV", "IIII").replace("IX", "VIIII")
+    roman_string = roman_string.replace("XL", "XXXX").replace("XC", "LXXXX")
+    roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
+    for char in roman_string:
+        num += roman_dict[char]
+    print(num)
