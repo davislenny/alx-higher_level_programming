f026e3f3c2b96c0709bb3653034d079be2057568
davislenny <davidlmwangi7@gmail.com>
2022-12-09 08:00:24 -0800

    Add 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
 
     roman_dict = {
             "I": 1,
@@ -16,4 +18,4 @@ def roman_to_int(roman_string):
     roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
     for char in roman_string:
         num += roman_dict[char]
-    print(num)
+    return num
