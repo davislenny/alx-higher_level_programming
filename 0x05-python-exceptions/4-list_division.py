64a875c6ca50eb5d6b280e654a1405c01da495d3
davislenny <davidlmwangi7@gmail.com>
2022-12-23 11:16:10 -0800

    Add 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,6 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     i = 0
     while i < list_length:
+        div = 0
         try:
             div = my_list_1[i] / my_list_2[i]
         except (ValueError, TypeError):
