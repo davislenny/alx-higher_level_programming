68f01433de4916c3f12c47c8f4a78350028cc0b3
davislenny <davidlmwangi7@gmail.com>
2022-12-25 04:22:42 -0800

    Add 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     var:
         size: square size of type int
      """
-    
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
