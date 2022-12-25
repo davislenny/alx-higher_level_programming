d94cedbf0eb4b77135c7610f5672c96b10f88477
davislenny <davidlmwangi7@gmail.com>
2022-12-25 04:32:55 -0800

    Add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Initiate class named Square"""
+
+class Square:
+    """
+    Define class
+    var:
+        size: square size of type int
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+"""Define a Public instance method
+    that returns the area of the square"""
+
+
+        def area(self):
+            return self.size**2
