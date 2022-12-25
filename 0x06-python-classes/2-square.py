1ace049bf64072d67dcd7aa98cbe60eddcae19d7
davislenny <davidlmwangi7@gmail.com>
2022-12-25 04:16:28 -0800

    Add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""Initiate a class name Square"""
+
+
+class Square:
+    """Initiate the class
+    var:
+        size: square size of type int
+     """
+    
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
