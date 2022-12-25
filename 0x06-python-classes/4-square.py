f1d46061f56dc373d7c33f42a5c24569bad06ea2
davislenny <davidlmwangi7@gmail.com>
2022-12-25 06:05:28 -0800

    Add 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,15 +8,6 @@ class Square:
     size: squar size of type int"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
-
-    def area(self):
-        """returs the area of the square"""
-        return self.__size**2
 
     @property
     def size(self):
@@ -25,5 +16,15 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """sets the size value"""
+        """sets the size value
+        rases error
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
+
+    def area(self):
+        """returs the area of the square"""
+        return self.size ** 2
