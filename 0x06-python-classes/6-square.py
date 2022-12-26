3cb2ae3b538b321f608ddd924c5b36cf9c989262
davislenny <davidlmwangi7@gmail.com>
2022-12-26 06:58:50 -0800

    Addd 6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Initiate a class named Square"""
+
+
+class Square:
+    """
+    size: length of the square
+    area: area of the square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >=0')
+        self.__size = size
+
+    def area(self):
+        """returns the area of square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """returns the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size of square to value"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """prints the square with the character #"""
+        print("\n".join(["#" * self.__size for rows in range(self._-size)]))
+
+    @property
+    def position(self):
+        """returns the position of the square"""
+        return self.__position
+
+    def position(self, value):
+        if not isinstance(value, tuple) or \
+        len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
