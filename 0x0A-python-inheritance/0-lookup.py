cc58d260f10151e69967862b3c132ffddedac396
davislenny <davidlmwangi7@gmail.com>
2023-01-10 00:07:58 -0800

    Add 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The '0-lookup' module
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the attributes and methods
+    of the python object 'obj'
+    """
+    return dir(obj)
