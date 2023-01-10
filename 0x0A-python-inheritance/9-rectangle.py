e722486c17a9da4f61310406057d02a709a1ab55
davislenny <davidlmwangi7@gmail.com>
2023-01-10 03:46:02 -0800

    Add 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,11 +15,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-        def area(self):
-            """Implementing area: area of the rectangle"""
-            return self.__width * self.__height
+    def area(self):
+        """Implementing area: area of the rectangle"""
+        return self.__width * self.__height
 
-        def __str__(self):
-            """returns [Rectangle] <width>/<height>"""
-            return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
-                                                self.__width, self.__height)
+    def __str__(self):
+        """returns [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__width, self.__height)
