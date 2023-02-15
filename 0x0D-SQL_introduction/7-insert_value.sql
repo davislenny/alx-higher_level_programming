77ec22b1ea6427f463b83a1f25a426adb47c415f
David <davidlmwangi7@gmail.com>
2023-02-15 20:24:51 +0300

    Add 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Sets new row in the table first_table (database hbtn_0c_0)
-INSERT INTO first_table (id, name) VALUES (89, 'Best Scool');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
