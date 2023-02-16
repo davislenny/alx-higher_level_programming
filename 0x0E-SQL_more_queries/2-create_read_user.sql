3df91f6d2ac5fc41b6f50f8dccce65d472c211a9
David <davidlmwangi7@gmail.com>
2023-02-16 16:09:08 +0300

    Add 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates the database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user user_Od_2 with password user_Od_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Give the user readonly privilege
