0a63dacaed4acd62f9829bde3afd588a4808e844
David <davidlmwangi7@gmail.com>
2023-02-16 20:22:51 +0300

    add 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- Display all comedy shows
+SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND g.name = "Comedy"
+ORDER BY s.title ASC;
